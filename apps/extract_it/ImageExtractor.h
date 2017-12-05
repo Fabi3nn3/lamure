245d8deddf52e4f7e86f90baa1cbf3a8453e32f6
Philipp Seltmann <philipp.seltmann@uni-weimar.de>
2017-12-05 10:58:13 +0100

    Add comments

--- apps/extract_it/ImageExtractor.h
+++ apps/extract_it/ImageExtractor.h
@@ -11,9 +11,22 @@
 
 typedef std::array<uint64_t, 2> pixel_pos;
 
+/**
+ * ImageExtractor extracts from a given binary image file (virtual texture)
+ * a subimage specified by the uper-left and lower-right corners.
+ *
+ * This class is the opposite to the stitch_it application to check whether
+ * the stitched image is correct while the file size exceeds the limits of gimp.
+ */
 class ImageExtractor {
 public:
     ImageExtractor(const std::string &input_path, const std::string &output_path);
+
+    /**
+     * Extract subimage data based on the start and end pixel from the given input file.
+     * @param start_pixel Coordinates of the upper left corner pixel of the subimage
+     * @param end_pixel Coordinates of the lower right corner pixel of the subimage
+     */
     void extract(const pixel_pos &start_pixel, const pixel_pos &end_pixel);
 
 private:
@@ -25,8 +38,28 @@ private:
     std::string input_path;
     std::string output_path;
 
+    /**
+     * Extracts the information about the virtual texture from the file name.
+     */
     void get_vt_dimensions();
+
+
+    /**
+     * Depending on the virtual texture size and the start and end pixel,
+     * it creates the output file name.
+     * @param start_pixel Coordinates of the upper left corner pixel of the subimage
+     * @param end_pixel Coordinates of the lower right corner pixel of the subimage
+     * @return output file name
+     */
     std::string generate_output_file_name(const pixel_pos &start_pixel, const pixel_pos &end_pixel);
+
+
+    /**
+     * Checks of the start and end pixels are in the bounds of the virtual texture.
+     * If not, it throws an InvalidArgument exception.
+     * @param start Coordinates of the upper left corner pixel of the subimage
+     * @param end Coordinates of the lower right corner pixel of the subimage
+     */
     void pixel_validity(const pixel_pos &start, const pixel_pos &end);
 };
 
